f2edad2d6e626a53deb5ddc6f9baa268ddef27b6
Luke <lucas.gab1010@gmail.com>
2025-09-09 12:31:06 -0300

    Adicionando headers no controller github

--- src/controllers/portifolioController.js
+++ src/controllers/portifolioController.js
@@ -1,16 +1,28 @@
-const path = require("path")
-const axios = require("axios")
+const path = require("path");
+const axios = require("axios");
 const basePath = path.join(__dirname, "../../public");
+require("dotenv").config();
+const token = process.env.GITHUB_TOKEN
 
 module.exports = class portifolioController {
-    static index (req, res) {
-        res.sendFile(`${basePath}/html/index.html`)
-    }
+  static index(req, res) {
+    res.sendFile(`${basePath}/html/index.html`);
+  }
 
-    static async github (req,res){
-        const username = "Lukeehz"
-        const response = await axios.get(`https://api.github.com/users/${username}/repos`);
-        const repos = response.data.map(repo => repo.name)
-        res.json(repos)
-    }
-}
+  static async github(req, res) {
+    const username = "Lukeehz";
+    const response = await axios.get(
+      `https://api.github.com/users/${username}/repos`,
+      {
+        headers:{
+            "User-Agent": "portifolio",
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": `token ${token}`
+        }
+      }
+    );
+
+    const repos = response.data.map((repo) => repo.name);
+    res.json(repos);
+  }
+};
